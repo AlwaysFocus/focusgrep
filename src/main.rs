8ac56dbe30243070736a98ea3672b11fc13d887c
Elijah Whalen <eliwhalen3@gmail.com>
2022-07-16 12:44:40 -0600

    Finished refactoring, fully functional

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,16 @@
+use focusgrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+    if let Err(e) = focusgrep::run(config) {
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {} in {}", config.query, config.filename);
-
-    let contents: String = fs::read_to_string(config.filename)?;
-    println!("Retrieved contents: \n {}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        // We will modify this to use lifetimes instead of cloning later on.
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-            query: query.to_string(),
-            filename: filename.to_string(),
-        })
-    }
 }
